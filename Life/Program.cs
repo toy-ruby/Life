5cdffbd740e6f55b7a8c9245125b088cce9d0fc7
toy-ruby <toy.ruby@gmail.com>
2017-01-22 12:36:07 -0600

    first commit

--- Life/Program.cs
+++ Life/Program.cs
@@ -10,6 +10,39 @@ namespace Life
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter grid size: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[,] currentGrid = new int[size, size];
+            int[,] nextGrid = new int[size, size];
+
+            initializeGrid(currentGrid);
+            printGrid(currentGrid);
+            
+        }
+
+        static void initializeGrid(int[,] grid)
+        {
+            for (int i = 0; i < Math.Sqrt(grid.Length); i++)
+            {
+                for (int j = 0; j < Math.Sqrt(grid.Length); j++)
+                {
+                    grid[i, j] = 1;
+                }
+            }
+        }
+
+        static void printGrid(int[,] grid)
+        {
+            // Print out grid
+            for (int i = 0; i < Math.Sqrt(grid.Length); i++)
+            {
+                for (int j = 0; j < Math.Sqrt(grid.Length); j++)
+                {
+                    Console.Write(grid[i, j]);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
         }
     }
 }
